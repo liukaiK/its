4eddacf07d4be15f232172b1a4420017d30579a9
liukai <zhizhufan@foxmail.com>
2021-01-04 17:08:05 +0800

    add: SpringSecurity配置类

---
+++ web/src/main/java/cn/com/goodlan/its/security/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package cn.com.goodlan.its.security;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * SpringSecurity核心配置类
+ *
+ * @author liukai
+ */
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+}
