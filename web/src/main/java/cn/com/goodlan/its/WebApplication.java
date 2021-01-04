fa66325001cea853b715aa62e11be3a8a1515cb1
liukai <zhizhufan@foxmail.com>
2021-01-04 16:27:17 +0800

    init web module

---
+++ web/src/main/java/cn/com/goodlan/its/WebApplication.java
@@ -0,0 +1,18 @@
+package cn.com.goodlan.its;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主程序入口
+ *
+ * @author liukai
+ */
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
