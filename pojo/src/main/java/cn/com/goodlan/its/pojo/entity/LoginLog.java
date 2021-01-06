b1e66402a9a1378ba87debac28e4fb5e9a394efd
liukai <zhizhufan@foxmail.com>
2021-01-06 10:34:28 +0800

    add: 修改登录日志实体

--- pojo/src/main/java/cn/com/goodlan/its/pojo/entity/LoginLog.java
+++ pojo/src/main/java/cn/com/goodlan/its/pojo/entity/LoginLog.java
@@ -1,7 +1,5 @@
 package cn.com.goodlan.its.pojo.entity;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
@@ -13,33 +11,89 @@ import java.time.LocalDateTime;
  *
  * @author liukai
  */
-@Setter
-@Getter
 @Entity
 @Table(name = "sys_login_log")
 public class LoginLog {
 
 
-    @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
     private String id;
 
-    @Column(nullable = false)
     private String username;
 
-    @Column(nullable = false)
     private String ip;
 
     private String browser;
 
     private String os;
 
-    @Column(columnDefinition = "tinyint")
     private Integer message;
 
-    @Column(nullable = false)
     private LocalDateTime loginTime;
+
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Column(nullable = false)
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Column(nullable = false)
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getBrowser() {
+        return browser;
+    }
+
+    public void setBrowser(String browser) {
+        this.browser = browser;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    @Column(columnDefinition = "tinyint")
+    public Integer getMessage() {
+        return message;
+    }
+
+
+    public void setMessage(Integer message) {
+        this.message = message;
+    }
+
+    @Column(nullable = false)
+    public LocalDateTime getLoginTime() {
+        return loginTime;
+    }
+
+    public void setLoginTime(LocalDateTime loginTime) {
+        this.loginTime = loginTime;
+    }
 
 
 }
