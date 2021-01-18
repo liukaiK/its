b8585c260b02eafdab9621f3213615ccf19acf05
liukai <zhizhufan@foxmail.com>
2021-01-18 10:25:53 +0800

    feat: 区域实体

---
+++ pojo/src/main/java/cn/com/goodlan/its/pojo/entity/Region.java
@@ -0,0 +1,43 @@
+package cn.com.goodlan.its.pojo.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 区域实体
+ *
+ * @author liukai
+ */
+@Entity
+@Table(name = "sys_region")
+public class Region extends BaseEntity {
+
+
+    private String id;
+
+    private String name;
+
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
