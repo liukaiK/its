22038016160b1d21db1864cc4aecc574acb393a0
liukai <zhizhufan@foxmail.com>
2021-01-19 10:43:26 +0800

    feat: 摄像头管理

--- web/src/main/java/cn/com/goodlan/its/web/controller/system/camera/CameraController.java
+++ web/src/main/java/cn/com/goodlan/its/web/controller/system/camera/CameraController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,6 +42,16 @@ public class CameraController {
     @PreAuthorize("hasAuthority('system:camera:search')")
     public Page<CameraVO> search(CameraDTO cameraDTO, @PageableDefault Pageable pageable) {
         return cameraService.search(cameraDTO, pageable);
+    }
+
+
+    /**
+     * 跳转到新增页面
+     */
+    @GetMapping("/add")
+    @PreAuthorize("hasAuthority('system:camera:add')")
+    public ModelAndView add(Model model) {
+        return new ModelAndView("system/camera/add");
     }
 
 
