35736a07220c92b3e43e22ce6f3352bd827e3506
liukai <zhizhufan@foxmail.com>
2021-01-22 14:45:00 +0800

    feat: 更新SQL

--- pojo/src/main/java/cn/com/goodlan/its/pojo/entity/Event.java
+++ pojo/src/main/java/cn/com/goodlan/its/pojo/entity/Event.java
@@ -73,7 +73,7 @@ public class Event extends BaseEntity {
         this.place = place;
     }
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "camera_id")
     public Camera getCamera() {
         return camera;
