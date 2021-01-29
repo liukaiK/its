028f56fae91ad07f64782ad66231eb1d80659e4a
liukai <zhizhufan@foxmail.com>
2021-01-29 18:23:16 +0800

    feat: 增加实体

---
+++ pojo/src/main/java/cn/com/goodlan/its/pojo/entity/Record.java
@@ -0,0 +1,68 @@
+package cn.com.goodlan.its.pojo.entity;
+
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * 记录实体
+ *
+ * @author liukai
+ */
+@Entity
+@Table(name = "eve_record")
+public class Record extends BaseEntity {
+
+    private String id;
+
+    private Event event;
+
+    private Score score;
+
+    private College college;
+
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "score_id")
+    public Score getScore() {
+        return score;
+    }
+
+    public void setScore(Score score) {
+        this.score = score;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "college_id")
+    public College getCollege() {
+        return college;
+    }
+
+    public void setCollege(College college) {
+        this.college = college;
+    }
+
+
+}
