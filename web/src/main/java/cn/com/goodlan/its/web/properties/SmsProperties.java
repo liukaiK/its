dc2fd786cb6275a89f51a178790f9a3f7f9d83d7
liukai <zhizhufan@foxmail.com>
2021-02-07 14:05:38 +0800

    feat: 增加发短信的配置

---
+++ web/src/main/java/cn/com/goodlan/its/web/properties/SmsProperties.java
@@ -0,0 +1,30 @@
+package cn.com.goodlan.its.web.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "sms")
+public class SmsProperties {
+
+    /**
+     * 应用id
+     */
+    private String applicationId;
+
+    /**
+     * 应用密码
+     */
+    private String applicationPassword;
+
+    /**
+     * 发送短信的url
+     */
+    private String sendSmsUrl;
+
+
+}
