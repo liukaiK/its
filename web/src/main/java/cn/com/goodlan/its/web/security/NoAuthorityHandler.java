0818ff030fcb52fbd49f270ea1e252f1c95526f8
liukai <zhizhufan@foxmail.com>
2021-02-09 12:22:36 +0800

    feat: 处理异常

--- web/src/main/java/cn/com/goodlan/its/web/security/NoAuthorityHandler.java
+++ web/src/main/java/cn/com/goodlan/its/web/security/NoAuthorityHandler.java
@@ -1,6 +1,12 @@
 package cn.com.goodlan.its.web.security;
 
+import cn.com.goodlan.its.common.constant.SystemConstant;
+import cn.com.goodlan.its.common.util.HttpUtil;
+import cn.com.goodlan.its.pojo.Result;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -9,6 +15,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 
 /**
@@ -20,28 +27,23 @@ import java.io.IOException;
 @Component
 public class NoAuthorityHandler implements AccessDeniedHandler {
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        log.error("没有权限", accessDeniedException);
-//        if (HttpUtil.isAjaxRequest(request)) {
-//            response.setCharacterEncoding("UTF-8");
-//            response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-//            response.setStatus(HttpServletResponse.SC_OK);
-//            PrintWriter writer = response.getWriter();
-//            if (authException instanceof UsernameMoreThanOneException) {
-//                Result responseBody = Result.fail(500, "用户数据出现异常,请联系管理员");
-//                writer.write(objectMapper.writeValueAsString(responseBody));
-//                writer.close();
-//                return;
-//            }
-//
-//            Result responseBody = Result.fail(403, "认证已失效,需要重新登录");
-//
-//            writer.write(objectMapper.writeValueAsString(responseBody));
-//            writer.close();
-//        } else {
-//            response.sendRedirect(SystemConstant.LOGIN_PAGE);
-//        }
+        log.error("访问被拒绝", accessDeniedException);
+        if (HttpUtil.isAjaxRequest(request)) {
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setStatus(HttpServletResponse.SC_OK);
+            PrintWriter writer = response.getWriter();
+            Result responseBody = Result.fail(500, "认证已失效,需要重新登录");
+            writer.write(objectMapper.writeValueAsString(responseBody));
+            writer.close();
+        } else {
+            response.sendRedirect(SystemConstant.LOGIN_PAGE);
+        }
 
     }
 
