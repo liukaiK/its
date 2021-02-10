da0ee0b80b34a2db8fae53d5461404ca0d522b4f
liukai <zhizhufan@foxmail.com>
2021-02-10 09:32:03 +0800

    feat: 删除打印

--- service/src/main/java/cn/com/goodlan/its/service/event/approval/RabbitObtainEventImpl.java
+++ service/src/main/java/cn/com/goodlan/its/service/event/approval/RabbitObtainEventImpl.java
@@ -46,7 +46,6 @@ public class RabbitObtainEventImpl {
     @RabbitHandler
     @RabbitListener(queuesToDeclare = @Queue(name = "its.traffic.event", durable = "true"))
     public synchronized void obtainEvent(String message) throws JsonProcessingException {
-        System.out.println(message);
         String content = StringEscapeUtils.unescapeJava(message);
         if (StringUtils.startsWithIgnoreCase(content, "\"")) {
             content = content.substring(1, content.length() - 1);
