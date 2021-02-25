dc74561278bd14e0a1223e1feea948d69976aa33
liukai <zhizhufan@foxmail.com>
2021-02-25 09:43:14 +0800

    feat: 角色管理后台校验

--- pojo/src/main/java/cn/com/goodlan/its/pojo/dto/RoleDTO.java
+++ pojo/src/main/java/cn/com/goodlan/its/pojo/dto/RoleDTO.java
@@ -2,12 +2,14 @@ package cn.com.goodlan.its.pojo.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class RoleDTO {
 
-
     private String id;
 
+    @NotBlank(message = "请输入角色名称")
     private String roleName;
 
     private String remark;
