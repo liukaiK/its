4b53dd74c7b2a0c722fad6bf469186e4ba4c50c0
liukai <zhizhufan@foxmail.com>
2021-03-11 15:01:07 +0800

    feat: 增加Controller

---
+++ api/src/main/java/cn/com/goodlan/its/api/VehicleController.java
@@ -0,0 +1,19 @@
+package cn.com.goodlan.its.api;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VehicleController {
+
+
+
+
+
+    @PutMapping("/vehicle")
+    public void save() {
+
+    }
+
+
+}
