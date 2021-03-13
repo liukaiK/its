a9dc54f864587f659b6110994c477beb50049ad5
liukai <zhizhufan@foxmail.com>
2021-03-13 11:48:35 +0800

    feat: 短信文字

---
+++ web/src/main/java/cn/com/goodlan/its/web/sms/SmsServiceImpl.java
@@ -0,0 +1,11 @@
+package cn.com.goodlan.its.web.sms;
+
+public class SmsServiceImpl implements SmsService {
+
+    String message1 = "您好，您的车辆 XXXX（车牌号）于2021年3月13日11:25:39（违规时间）在XXX（违规地点）超速/违停（违规分类），系统对您于警告处理。";
+
+    String message2 = "您好，您的车辆 XXXX（车牌号）于2021年3月13日11:25:39（违规时间）在XXX（违规地点）超速/违停（违规分类），扣分XX（分）。您已经尾骨超过4次被系统拉黑。";
+
+    String message3 = "您好，您的车辆 XXXX（车牌号）于2021年3月13日11:25:39（违规时间）在XXX（违规地点）超速/违停（违规分类），扣分XX（分）。违规超过4次将被进行拉黑处理。";
+
+}
